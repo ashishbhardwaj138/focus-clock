3de5d2189f9b0662e471dc3a34bdd1acdd09fef2
Ashish Bhardwaj <ashubhardwaj138@gmail.com>
2026-09-22 14:02:42 +0530

    Archive legacy setup scripts

--- uninstall.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = 'SilentlyContinue'
-Get-Process FocusClock -ErrorAction SilentlyContinue | Stop-Process -Force
-Remove-Item (Join-Path $env:APPDATA 'Microsoft\Windows\Start Menu\Programs\Startup\Focus Clock.lnk') -Force
-Remove-Item (Join-Path $env:APPDATA 'Microsoft\Windows\Start Menu\Programs\Focus Clock.lnk') -Force
-Remove-Item (Join-Path $env:LOCALAPPDATA 'FocusClock') -Recurse -Force
-Write-Host 'Focus Clock removed.'
-
